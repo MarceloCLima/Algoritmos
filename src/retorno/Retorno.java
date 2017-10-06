a92d0d387af2257db2468b70d1df25b4e3a0ea59
Aluno <Aluno@172.32.1.32>
2017-10-05 21:57:40 -0300

    Criação de função com retorno. Criação de um pacote com uma classe que possui o código da aula 28.

--- src/retorno/Retorno.java
+++ src/retorno/Retorno.java
@@ -1,10 +1,28 @@
-
 package retorno;
 
 public class Retorno {
 
-    public static void main(String[] args) {
-        
+    public static void quadrado(double n) {
+        double r = n * n;
     }
     
+    public static double metade(double v){
+        double r = v/2;
+        return r;
+    }
+
+    public static void main(String[] args) {
+
+        // Calculando a raiz quadrada
+        double raiz;
+
+        System.out.println("raiz: " + Math.sqrt(15));
+        
+        quadrado(15);
+
+        System.out.println("Metade: " + metade(15));
+        
+
+    }
+
 }
